83555d46175adccf84c3640957ba6cd42567c341
Vojislav Trujkic <voizlav@gmail.com>
2024-06-28 13:46:28 +0200

    Add Average of Positive Number

--- Part8/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ Part8/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,10 +1,27 @@
-
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0, count = 0;
+        while (true) {
+            String input = scanner.nextLine();
+            Integer number = Integer.parseInt(input);
+            if (number == 0) {
+                scanner.close();
+                break;
+            }
+            if (number > 0) {
+                sum += number;
+                count++;
+            }
+        }
+        if (count == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            double result = (double) sum / count;
+            System.out.println(result);
+        }
     }
 }
