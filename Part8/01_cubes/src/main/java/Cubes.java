87a8f6bb660c9f5952900a5d5402f128e9bd9919
Vojislav Trujkic <voizlav@gmail.com>
2024-06-28 13:29:41 +0200

    Add cubes

--- Part8/01_cubes/src/main/java/Cubes.java
+++ Part8/01_cubes/src/main/java/Cubes.java
@@ -6,5 +6,14 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                scanner.close();
+                break;
+            }
+            Integer number = Integer.parseInt(input);
+            System.out.println(number * number * number);
+        }
     }
 }
