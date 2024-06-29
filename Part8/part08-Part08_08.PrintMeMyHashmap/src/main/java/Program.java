06c64e30dfcea4f65331b45b225d8be75b357c93
Vojislav Trujkic <voizlav@gmail.com>
2024-06-29 12:55:02 +0200

    Initial Print me my hashmap

---
+++ Part8/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -0,0 +1,19 @@
+
+import java.util.HashMap;
+
+public class Program {
+
+    public static void main(String[] args) {
+        // Test your program here!
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("f.e", "for example");
+        hashmap.put("etc.", "and so on");
+        hashmap.put("i.e", "more precisely");
+
+        printKeys(hashmap);
+        System.out.println("---");
+        printKeysWhere(hashmap, "i");
+        System.out.println("---");
+        printValuesOfKeysWhere(hashmap, ".e");
+    }
+}
