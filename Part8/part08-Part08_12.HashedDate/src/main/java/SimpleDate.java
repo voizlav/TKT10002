29fc6a19722405b9b68a9ae00fdb96c67c65582a
Vojislav Trujkic <voizlav@gmail.com>
2024-07-07 18:53:43 +0200

    Update Hash Date

--- Part8/part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
+++ Part8/part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
@@ -51,4 +51,24 @@ public class SimpleDate {
         return this.year - other.year - yearRemoved;
     }
 
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof SimpleDate)) {
+            return false;
+        }
+
+        SimpleDate date = (SimpleDate) obj;
+
+        return date.day == this.day
+                && date.month == this.month
+                && date.year == this.year;
+    }
+
+    public int hashCode() {
+        String hash = String.format("%d%d%d", this.day, this.month, this.year);
+        return Integer.parseInt(hash);
+    }
 }
