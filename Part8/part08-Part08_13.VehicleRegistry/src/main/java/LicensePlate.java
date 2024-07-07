fcd5eaed92ad174a0678db7c6c8e56b288f2eb8a
Vojislav Trujkic <voizlav@gmail.com>
2024-07-07 20:26:52 +0200

    Update License Plate - Equals and hashCode for the LicensePlate class

--- Part8/part08-Part08_13.VehicleRegistry/src/main/java/LicensePlate.java
+++ Part8/part08-Part08_13.VehicleRegistry/src/main/java/LicensePlate.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 public class LicensePlate {
     // don't modify existing parts of this class
 
-    // these instance variables have been defined as final, meaning 
+    // these instance variables have been defined as final, meaning
     // that once set, their value can't be changed
     private final String liNumber;
     private final String country;
@@ -19,4 +19,21 @@ public class LicensePlate {
         return country + " " + liNumber;
     }
 
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (this.getClass() != obj.getClass() || obj == null) {
+            return false;
+        }
+
+        LicensePlate li = (LicensePlate) obj;
+
+        return this.country == li.country && this.liNumber == li.liNumber;
+    }
+
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
 }
