97f0f32cb62350f08d8931a5d70aa7c84774632d
Vojislav Trujkic <voizlav@gmail.com>
2024-07-16 17:02:26 +0200

    Update Person and Subclasses - Teacher

--- Part9/part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ Part9/part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -4,9 +4,18 @@ public class Main {
     public static void main(String[] args) {
         // write your test code here
 
+        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+        System.out.println(ada);
+        System.out.println(esko);
+
         Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-        System.out.println(ollie);
-        ollie.study();
+
+        int i = 0;
+        while (i < 25) {
+            ollie.study();
+            i++;
+        }
         System.out.println(ollie);
     }
 }
